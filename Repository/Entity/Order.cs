c632e1258385c8f10c4c15951dbe2c6e297e1f6b
Lâm Tấn Phú <phultse182607@fpt.edu.vn>
2025-03-03 15:16:24 +0700

    Reduce project size

---
+++ Repository/Entity/Order.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PRN222_Group_Project.Models.Entities
+{
+    public class Order
+    {
+        [Key]
+        public Guid OrderId { get; set; }
+        public Guid FKFeedbackId { get; set; }
+        public Feedback Feedback { get; set; }
+        public Guid FKProfileId { get; set; }
+        public ChildrenProfile Profile { get; set; }
+        public int TotalAmount { get; set; }
+        public int TotalOrderPrice { get; set; }
+        public DateTime PurchaseDate { get; set; }
+        public int Status { get; set; }
+    }
+}
